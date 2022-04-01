6001f980be255ee3f9d7009a245338717a5f7c80
Kecskeméti.Levente <kiskecsoka@gmail.com>
2022-04-01 02:10:38 +0200

    Job Model Added.

---
+++ AKFAC0_HFT_2021222.Models/Job.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKFAC0_HFT_2021222.Models
+{
+	public class Job : Entity
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		[Column("Job_id", TypeName = "int")]
+		public override int Id { get; set; }
+
+		[Required]
+		[StringLength(240)]
+		public string Name { get; set; }
+
+		[Required]
+		[Range(1,90)]
+		public string Role { get; set; }
+
+		[NotMapped] // Not sure?
+		public virtual ICollection<Weapon> Weapons { get; set; }
+
+		[NotMapped]// Not sure?
+		public virtual ICollection<Armor> Armors { get; set; }
+
+		public Job()
+		{
+			this.Weapons = new HashSet<Weapon>();
+			this.Armors = new HashSet<Armor>();
+		}
+	}
+}
