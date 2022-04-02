6f667b90ce1abd64fd8c1186404633da85ff2d5e
Kecskeméti.Levente <kiskecsoka@gmail.com>
2022-04-02 02:09:05 +0200

    Weapon Repository Created

---
+++ AKFAC0_HFT_2021222.Repository/Interfaces/IWeaponRepository.cs
@@ -0,0 +1,13 @@
+﻿using AKFAC0_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKFAC0_HFT_2021222.Repository.Interfaces
+{
+	public interface IWeaponRepository : IRepository<Weapon>
+	{
+	}
+}
---
+++ AKFAC0_HFT_2021222.Repository/Repositories/WeaponRepository.cs
@@ -0,0 +1,16 @@
+﻿using AKFAC0_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKFAC0_HFT_2021222.Repository.Repositories
+{
+	internal class WeaponRepository : Repository<Weapon>
+	{
+		public WeaponRepository(JobDbContext ctx) : base(ctx)
+		{
+		}
+	}
+}
