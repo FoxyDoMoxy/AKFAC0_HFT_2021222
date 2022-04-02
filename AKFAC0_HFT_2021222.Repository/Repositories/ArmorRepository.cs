f6e24b7e9129509a52970655b7ac2190625e9b6b
Kecskeméti.Levente <kiskecsoka@gmail.com>
2022-04-02 02:08:56 +0200

    Armor Repository Created.

---
+++ AKFAC0_HFT_2021222.Repository/Repositories/ArmorRepository.cs
@@ -0,0 +1,16 @@
+﻿using AKFAC0_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKFAC0_HFT_2021222.Repository.Repositories
+{
+	public class ArmorRepository : Repository<Armor>
+	{
+		public ArmorRepository(JobDbContext ctx) : base(ctx)
+		{
+		}
+	}
+}
