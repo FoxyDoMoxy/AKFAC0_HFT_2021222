1c0bc868acc82291757067599f547cd75b79464f
Kecskeméti.Levente <kiskecsoka@gmail.com>
2022-04-26 00:04:59 +0200

    Testing methods

---
+++ AKFAC0_HFT_2021222.Test/ArmorTester.cs
@@ -0,0 +1,117 @@
+﻿using AKFAC0_HFT_2021222.Logic;
+using AKFAC0_HFT_2021222.Logic.Classes;
+using AKFAC0_HFT_2021222.Models;
+using AKFAC0_HFT_2021222.Repository;
+using AKFAC0_HFT_2021222.Repository.Interfaces;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKFAC0_HFT_2021222.Test
+{
+	[TestFixture]
+	public class ArmorTester
+	{
+
+		ArmorLogic ArmorLogic;
+		Mock<IRepository<Armor>> mockArmorRepo;
+
+		[SetUp]
+		public void Init()
+		{
+			//ARRANGE
+			mockArmorRepo = new Mock<IRepository<Armor>>();
+			mockArmorRepo.Setup(mr => mr.ReadAll()).Returns(new List<Armor>()
+			{
+				new Armor("AAAA",10,1),
+				new Armor("BBBB",100,1),
+				new Armor("CCCC",1000,1),
+				new Armor("DDDD",0,1),
+			}.AsQueryable());
+
+			ArmorLogic = new ArmorLogic(mockArmorRepo.Object);
+
+		}
+
+		//create test
+		[Test]
+		public void CreateArmorTestWithCorrectName()
+		{
+			var armor = new Armor() { Name = "Test Mage"};
+			//ACT
+			ArmorLogic.Create(armor);
+
+			//ASSERT
+			mockArmorRepo.Verify(r => r.Create(armor), Times.Once);
+
+		}
+
+		[Test]
+		public void CreateArmorTestWithInCorrectName()
+		{
+			var armor = new Armor() { Name = "?:asdds" };
+			//ACT
+			try
+			{
+				ArmorLogic.Create(armor);
+			}
+			catch
+			{
+
+
+			}
+
+			//ASSERT
+			mockArmorRepo.Verify(r => r.Create(armor), Times.Never);
+
+		}
+
+
+		[Test]
+		public void CreateArmorTestWithCorrectLenghtName()
+		{
+			var armor = new Armor() { Name = "VAlamiii"};
+			//ACT
+			ArmorLogic.Create(armor);
+
+			//ASSERT
+			mockArmorRepo.Verify(r => r.Create(armor), Times.Once);
+
+		}
+
+		[Test]
+		public void CreateArmorTestWithInCorrectLenghtName()
+		{
+			var armor = new Armor() { Name = "a"};
+			//ACT
+			try
+			{
+				
+				ArmorLogic.Create(armor);
+			}
+			catch
+			{
+
+
+			}
+
+			//ASSERT
+			mockArmorRepo.Verify(r => r.Create(armor), Times.Never);
+
+		}
+
+		[Test]
+		public void ReadTest()
+		{
+			Armor result = ArmorLogic.Read(1);
+
+			//Assert
+
+			Assert.That(result.Name, Is.EqualTo("BBBB"));
+		}
+	}
+}
---
+++ AKFAC0_HFT_2021222.Test/JobTester.cs
@@ -0,0 +1,115 @@
+﻿using AKFAC0_HFT_2021222.Logic;
+using AKFAC0_HFT_2021222.Models;
+using AKFAC0_HFT_2021222.Repository;
+using AKFAC0_HFT_2021222.Repository.Interfaces;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKFAC0_HFT_2021222.Test
+{
+	[TestFixture]
+	public class JobTester
+	{
+
+        JobLogic joblogic;
+        Mock<IRepository<Job>> mockJobRepo;
+
+        [SetUp]
+        public void Init()
+        {
+            //ARRANGE
+            mockJobRepo = new Mock<IRepository<Job>>();
+            mockJobRepo.Setup(mr => mr.ReadAll()).Returns(new List<Job>()
+            {
+                new Job(0,"AAAA","TANK"),
+                new Job(1,"BBBB","HEALER"),
+                new Job(2,"CCCC","DPS"),
+                new Job(3,"DDDD","DPS"),
+            }.AsQueryable());
+
+            joblogic = new JobLogic(mockJobRepo.Object);
+
+        }
+
+		//create test
+		[Test]
+		public void CreateJobTestWithCorrectRole()
+		{
+			var job = new Job() {Name="Test Mage",Role="DPS" };
+			//ACT
+			joblogic.Create(job);
+
+			//ASSERT
+			mockJobRepo.Verify(r => r.Create(job), Times.Once);
+
+		}
+
+		[Test]
+		public void CreateJobTestWithInCorrectRole()
+		{
+			var job = new Job() { Name = "Test Mage", Role = "Student" };
+			//ACT
+			try
+			{
+				joblogic.Create(job);
+			}
+			catch
+			{
+
+	
+			}
+
+			//ASSERT
+			mockJobRepo.Verify(r => r.Create(job), Times.Never);
+
+		}
+
+
+		[Test]
+		public void CreateJobTestWithCorrectLenghtName()
+		{
+			var job = new Job() { Name = "VAlamiii", Role = "DPS" };
+			//ACT
+			joblogic.Create(job);
+
+			//ASSERT
+			mockJobRepo.Verify(r => r.Create(job), Times.Once);
+
+		}
+
+		[Test]
+		public void CreateJobTestWithInCorrectLenghtName()
+		{
+			var job = new Job() { Name = "a", Role = "TANK" };
+			//ACT
+			try
+			{
+				joblogic.Create(job);
+			}
+			catch
+			{
+
+
+			}
+
+			//ASSERT
+			mockJobRepo.Verify(r => r.Create(job), Times.Never);
+
+		}
+
+		[Test]
+		public void ReadTest()
+		{
+			Job result = joblogic.Read(1);
+
+			//Assert
+
+			Assert.That(result.Name, Is.EqualTo("BBBB"));
+		}
+	}
+}
