cc3a8ca38224228da059936ccfa28db26ca43239
Kecskeméti.Levente <kiskecsoka@gmail.com>
2022-04-26 01:37:11 +0200

    RestService Setup

--- AKFAC0_HFT_2021222.Client/Program.cs
+++ AKFAC0_HFT_2021222.Client/Program.cs
@@ -1,114 +1,111 @@
 ﻿using System;
-using AKFAC0_HFT_2021222.Logic;
-using AKFAC0_HFT_2021222.Logic.Classes;
 using AKFAC0_HFT_2021222.Models;
-using AKFAC0_HFT_2021222.Repository;
-using AKFAC0_HFT_2021222.Repository.Repositories;
 using ConsoleTools;
 
 namespace AKFAC0_HFT_2021222.Client
 {
 	internal class Program
 	{
-        static JobLogic jobLogic;
-        static WeaponLogic weaponLogic;
-        static ArmorLogic armorlogic;
-        static void Create(string entity)
-        {
-            switch (entity)
-            {
-                case "Job":
-
-                    break;
-                case "Weapon":
-
-                    break;
-                case "Armor":
-
-                    break;
-            }
-            Console.ReadLine();
-        }
-        static void List(string entity)
-        {
-
-            switch (entity)
-			{
-                case "Job":
-                    var jobs = jobLogic.ReadAll();
-                    foreach (var item in jobs)
-                    {
-						Console.WriteLine(item.Id+"\t"+item.Name+"\t"+item.Role);
-                    }
-                    break;
-                case "Weapon":
-                    var weapons = weaponLogic.ReadAll();
-                    foreach (var item in weapons)
-                    {
-                        Console.WriteLine(item.Id + "\t" + item.Name + "\t" + item.BaseDamage);
-                    }
-                    break;
-                case "Armor":
-                    var armors = armorlogic.ReadAll();
-                    foreach (var item in armors)
-                    {
-                        Console.WriteLine(item.Id + "\t" + item.Name + "\t" + item.BaseDefense);
-                    }
-                    break;
-            }
-			Console.ReadLine();
-        }
-        static void Update(string entity)
-        {
-            Console.WriteLine(entity + " update");
-            Console.ReadLine();
-        }
-        static void Delete(string entity)
-        {
-            Console.WriteLine(entity + " delete");
-            Console.ReadLine();
-        }
-        static void Main(string[] args)
+        static RestService rest;
+		#region wip
+		static void Create(string entity)
 		{
-            var ctx = new JobDbContext();
-            
-            var jobRepo=new JobRepository(ctx);
-            var weaponRepo=new WeaponRepository(ctx);
-            var ArmorRepo=new ArmorRepository(ctx);
+			//switch (entity)
+			//{
+			//	case "Job":
 
-            jobLogic = new JobLogic(jobRepo);
-            weaponLogic = new WeaponLogic(weaponRepo);
-            armorlogic = new ArmorLogic(ArmorRepo);
+			//		break;
+			//	case "Weapon":
 
-            var jobSubMenu = new ConsoleMenu(args, level: 1)
-                 .Add("List", () => List("Job"))
-                 .Add("Create", () => Create("Job"))
-                 .Add("Delete", () => Delete("Job"))
-                 .Add("Update", () => Update("Job"))
-                 .Add("Exit", ConsoleMenu.Close);
+			//		break;
+			//	case "Armor":
 
-            var weaponSubMenu = new ConsoleMenu(args, level: 1)
-                 .Add("List", () => List("Weapon"))
-                 .Add("Create", () => Create("Weapon"))
-                 .Add("Delete", () => Delete("Weapon"))
-                 .Add("Update", () => Update("Weapon"))
-                 .Add("Exit", ConsoleMenu.Close);
+			//		break;
+			//}
+			//Console.ReadLine();
+		}
+		static void List(string entity)
+		{
 
-            var armorSubMenu = new ConsoleMenu(args, level: 1)
-                 .Add("List", () => List("Armor"))
-                 .Add("Create", () => Create("Armor"))
-                 .Add("Delete", () => Delete("Armor"))
-                 .Add("Update", () => Update("Armor"))
-                 .Add("Exit", ConsoleMenu.Close);
+			//switch (entity)
+			//{
+			//	case "Job":
+			//		var jobs = rest.Get<Job>("job");
+			//		foreach (var item in jobs)
+			//		{
+			//			Console.WriteLine(item.Id + "\t" + item.Name + "\t" + item.Role);
+			//		}
+			//		break;
+			//	case "Weapon":
+			//		var weapons = rest.Get<Weapon>("job");
+			//		foreach (var item in weapons)
+			//		{
+			//			Console.WriteLine(item.Id + "\t" + item.Name + "\t" + item.BaseDamage);
+			//		}
+			//		break;
+			//	case "Armor":
+			//		var armors = rest.Get<Armor>("job");
+			//		foreach (var item in armors)
+			//		{
+			//			Console.WriteLine(item.Id + "\t" + item.Name + "\t" + item.BaseDefense);
+			//		}
+			//		break;
+			//}
+			Console.ReadLine();
+		}
+		static void Update(string entity)
+		{
+			Console.WriteLine(entity + " update");
+			Console.ReadLine();
+		}
+		static void Delete(string entity)
+		{
+			Console.WriteLine(entity + " delete");
+			Console.ReadLine();
+		}
 
-            var menu = new ConsoleMenu(args, level: 0)
-                 .Add("Jobs", () => jobSubMenu.Show())
-                 .Add("Weapons", () => weaponSubMenu.Show())
-                 .Add("Armors", () => armorSubMenu.Show())
-                 .Add("Exit", ConsoleMenu.Close);
+		#endregion
+		static void Main(string[] args)
+		{
 
-            menu.Show();
+			rest = new RestService("http://localhost:53910/", "service");
 
-        }
+
+			var jobs = rest.Get<Job>("job");
+			foreach (var item in jobs)
+			{
+				Console.WriteLine(item.Id + "\t" + item.Name + "\t" + item.Role);
+			}
+
+			var jobSubMenu = new ConsoleMenu(args, level: 1)
+				 .Add("List", () => List("Job"))
+				 .Add("Create", () => Create("Job"))
+				 .Add("Delete", () => Delete("Job"))
+				 .Add("Update", () => Update("Job"))
+				 .Add("Exit", ConsoleMenu.Close);
+
+			var weaponSubMenu = new ConsoleMenu(args, level: 1)
+				 .Add("List", () => List("Weapon"))
+				 .Add("Create", () => Create("Weapon"))
+				 .Add("Delete", () => Delete("Weapon"))
+				 .Add("Update", () => Update("Weapon"))
+				 .Add("Exit", ConsoleMenu.Close);
+
+			var armorSubMenu = new ConsoleMenu(args, level: 1)
+				 .Add("List", () => List("Armor"))
+				 .Add("Create", () => Create("Armor"))
+				 .Add("Delete", () => Delete("Armor"))
+				 .Add("Update", () => Update("Armor"))
+				 .Add("Exit", ConsoleMenu.Close);
+
+			var menu = new ConsoleMenu(args, level: 0)
+				 .Add("Jobs", () => jobSubMenu.Show())
+				 .Add("Weapons", () => weaponSubMenu.Show())
+				 .Add("Armors", () => armorSubMenu.Show())
+				 .Add("Exit", ConsoleMenu.Close);
+
+			menu.Show();
+
+		}
 	}
 }
--- AKFAC0_HFT_2021222.Endpoint/Startup.cs
+++ AKFAC0_HFT_2021222.Endpoint/Startup.cs
@@ -5,7 +5,9 @@ using AKFAC0_HFT_2021222.Repository;
 using AKFAC0_HFT_2021222.Repository.Interfaces;
 using AKFAC0_HFT_2021222.Repository.Repositories;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -57,6 +59,15 @@ namespace AKFAC0_HFT_2021222.Endpoint
 				app.UseSwagger();
 				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AKFAC0_HFT_2021222.Endpoint v1"));
 			}
+
+			app.UseExceptionHandler(c => c.Run(async context =>
+			{
+				var exception = context.Features
+					.Get<IExceptionHandlerPathFeature>()
+					.Error;
+				var response = new { Msg = exception.Message };
+				await context.Response.WriteAsJsonAsync(response);
+			}));
 
 			app.UseRouting();
 
