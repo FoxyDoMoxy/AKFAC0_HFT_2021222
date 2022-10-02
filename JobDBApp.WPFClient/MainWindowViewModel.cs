1bb160a21f1468eb6d07dae0923ac709785b3ad6
Kecskeméti.Levente <kiskecsoka@gmail.com>
2022-10-02 15:57:26 +0200

    Added Base Main Window

---
+++ JobDBApp.WPFClient/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using AKFAC0_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobDBApp.WPFClient
+{
+	public class MainWindowViewModel
+	{
+		public RestCollection<Job> Jobs { get; set; }
+		public MainWindowViewModel()
+		{
+			Jobs = new RestCollection<Job>("http://localhost:30703/", "job");
+		}
+	}
+}
